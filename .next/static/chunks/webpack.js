c9380db12a6c0f0b37f7b754a411ae1ffd832777
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-08-15 12:31:42 +0530

    Fully configured getStatciPaths and getStaticProps with hardcoded data for now

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "99e6e479fe4903af"; }
+/******/ 		__webpack_require__.h = function() { return "f7f34383f044e656"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,49 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-export default function MeetupDetails() {
+export default function MeetupDetails(props) {
   return (
     <>
       <MeetupDetail
-        image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        image={props.meetupData.image}
         title="First meetup"
         address="Powder Gali num 5"
         description="Some example description"
       />
     </>
   );
+}
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "First meetup",
+        address: "Powder Gali num 5",
+        description: "Some example description",
+      },
+    },
+  };
 }
