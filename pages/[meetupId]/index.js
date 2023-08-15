46f0da548aed1e9b6d87efe942bd6eb4ab45e133
Sammy970 <jainsamyak2002.sj@gmail.com>
2023-08-15 11:48:47 +0530

    Added custom component and css

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <>
+      <MeetupDetail
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        title="First meetup"
+        address="Powder Gali num 5"
+        description="Some example description"
+      />
+    </>
+  );
+}
